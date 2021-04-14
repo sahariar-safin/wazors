6b0251a72bb471c4f16b3f47866b14786225c024
Shafin The Developer <sahariar350@gmail.com>
2021-04-15 05:00:30 +0600

    somethig is cooking

--- src/componants/Home/Header/Header.js
+++ src/componants/Home/Header/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
+import './Header.css';
+import HeaderImage from '../../../Images/finn-zoQJIGS5EmQ-unsplash.jpg';
 
 const Header = () => {
     return (
-        <main>
-            
+        <main className="homeHeader bg-light container-fluid">
+            <div class="row align-items-center justify-content-around">
+                <div class="col-4">
+                    <h1>We build your dream</h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vitae possimus qui?</p>
+                    <button className="btn btn-primary">Discover More</button>
+                </div>
+                <div class="col-4 mt-5">
+                    <img src={HeaderImage} className="img-fluid rounded" alt=""/>
+                </div>
+            </div>
         </main>
     );
 };
